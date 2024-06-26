b11bec3341c09a1a01be6c5ee1a160756cd114d4
DesislavaF <156531205+DesislavaF@users.noreply.github.com>
2024-06-26 07:26:21 +0300

    Commit component Footer

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import Header from './components/Header/Header';
+import Footer from './components/Footer/Footer';
+
 import './App.css';
 
 function App() {
   return (
     <div className="container">
       <Header />
+      <Footer />
     </div>
   );
 }
