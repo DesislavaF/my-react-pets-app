0670b28f19e95ec2a395755c4857181ce33bd4ca
DesislavaF <156531205+DesislavaF@users.noreply.github.com>
2024-06-28 07:42:14 +0300

    Commit categories

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
+import { Route, Switch } from 'react-router-dom';
+
 import Header from './components/Header/Header';
 import Footer from './components/Footer/Footer';
+import Categories from './components/Categories/Categories';
 
 import './App.css';
 
@@ -7,6 +10,12 @@ function App() {
   return (
     <div className="container">
       <Header />
+
+      <Switch>
+        <Route path="/" exact component={Categories} />
+        <Route path="/categories/:category"  component={Categories} />
+      </Switch>
+
       <Footer />
     </div>
   );
