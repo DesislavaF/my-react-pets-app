441085ccb0302c854390a1e0d0d5b76d5f8019c3
DesislavaF <156531205+DesislavaF@users.noreply.github.com>
2024-08-02 07:29:56 +0300

    add Error Boundary

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ import PetDetails from './components/PetDetails/PetDetails';
 import EditPetDetails from './components/EditPetDetails/EditPetDetails';
 import EditPet from './components/EditPet/EditPet';
 import CreatePet from './components/CreatePet/CreatePet';
+import CustomErrorBoundary from './components/CustomErrorBoundary/CustomErrorBoundary';
 import './App.css';
 import { auth } from './utils/firebase'; // Ensure this import is correct
 
@@ -19,7 +20,7 @@ function App() {
     const unsubscribe = auth.onAuthStateChanged(setUser);
     return () => unsubscribe(); // Cleanup subscription on unmount
   }, []);
-  
+
   const authInfo = {
     isAuthenticated: Boolean(user),
     username: user?.email,
@@ -27,21 +28,24 @@ function App() {
 
   return (
     <div className="container">
-      <Header {...authInfo}/>
-      <Switch>
-        <Route path="/" exact render={props => <Categories {...props} {...authInfo} />} />
-        <Route path="/categories/:category" render={props => <Categories {...props} {...authInfo} />} />
-        <Route path="/pets/details/:petId" exact render={props => <PetDetails {...props} {...authInfo} />} />
-        <Route path="/pets/details/:petId/edit" render={props => <EditPetDetails {...props} {...authInfo} />} />
-        <Route path="/pets/create" render={props => <CreatePet {...props} {...authInfo} />} />
-        <Route path="/pets/:petId/edit" render={props => <EditPet {...props} {...authInfo} />} />
-        <Route path="/register" render={props => <Register {...props} {...authInfo} />} />
-        <Route path="/login" render={props => <Login {...props} {...authInfo} />} />
-        <Route path="/logout" render={() => {
-          auth.signOut();
-          return <Redirect to="/" />;
-        }} />
-      </Switch>
+      <Header {...authInfo} />
+
+      <CustomErrorBoundary>
+        <Switch>
+          <Route path="/" exact render={props => <Categories {...props} {...authInfo} />} />
+          <Route path="/categories/:category" render={props => <Categories {...props} {...authInfo} />} />
+          <Route path="/pets/details/:petId" exact render={props => <PetDetails {...props} {...authInfo} />} />
+          <Route path="/pets/details/:petId/edit" render={props => <EditPetDetails {...props} {...authInfo} />} />
+          <Route path="/pets/create" render={props => <CreatePet {...props} {...authInfo} />} />
+          <Route path="/pets/:petId/edit" render={props => <EditPet {...props} {...authInfo} />} />
+          <Route path="/register" render={props => <Register {...props} {...authInfo} />} />
+          <Route path="/login" render={props => <Login {...props} {...authInfo} />} />
+          <Route path="/logout" render={() => {
+            auth.signOut();
+            return <Redirect to="/" />;
+          }} />
+        </Switch>
+      </CustomErrorBoundary>
       <Footer />
     </div>
   );
